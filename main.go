58cb6f7ba0d1d1af31772396de49bdeca26e9c48
Arlak Abdulloh <mtb730914@HQT23042800010.local>
2023-06-18 16:40:26 +0700

    use gorilla mux

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	app := http.NewServeMux()
+	app := mux.NewRouter()
 
 	// use default http you handle http method and handle parameter
-	app.HandleFunc("/hello", Hello)
-
+	app.HandleFunc("/hello/{id}", Hello).Methods(http.MethodGet)
 	http.ListenAndServe(":8000", app)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
 	fmt.Println(r.Method)
+	fmt.Println("id : " + id)
+
 }
